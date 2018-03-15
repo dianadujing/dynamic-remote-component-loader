f67e35f36244a47d05c0c0ce7fe604269eee993f
dianadujing <dujing140@126.com>
2018-03-15 20:22:03 +0800

    add description

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# dynamic-remote-component-loader
+# Dynamic Remote Component Loader 
+This project demonstrates how to get and load a remote component/module in an angular 4 app.
+
+#####How to run this project
+
+Install all dependencies:
+>npm install 
+
+Build project and start application on lite server:
+>npm run start
+
+
+#####Project Inspiration
+
+This is a sample project realized remote component loading based on Angular 2:
+http://plnkr.co/edit/IpQ2HRa7yBj2YKZdgBZl?p=preview&open=app%2Fapp.component.ts
+https://stackoverflow.com/questions/38883065/angular2-convert-dynamic-component-loading-from-rc-4-to-rc5/38883838#38883838
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,24 +20,19 @@ export class AppComponent {
   }
 
   ngAfterViewInit(){
-    // const url = 'https://gist.githubusercontent.com/johnlindquist/90c0a12814939738809ae0dceacdcf93/raw/e95c3204af1335693a45d65dbb61162824ad5ab8/loadMe.ts';
-    const url = 'https://gist.githubusercontent.com/dianadujing/a7bbbf191349182e1d459286dba0282f/raw/c23281f8c5fabb10ab9d144489316919e4233d11/module.ts';
+    
+    const url = 'https://gist.githubusercontent.com/dianadujing/a7bbbf191349182e1d459286dba0282f/raw/c23281f8c5fabb10ab9d144489316919e4233d11/app.module.ts';
     console.log(System.import(url));
-    // System.import(url).then((comp:ElementRef) => {this.putStuffHere.element.nativeElement.style.color = 'red';});
+    
     const importer = (url:any) => Observable.fromPromise(System.import(url));
-    // importer(url).subscribe((component) => );
     console.log('importer:', importer);
     importer(url)
-    // .toPromise().then(component =>{console.log('component:', component)});
       .subscribe((modules) => {
         console.log('modules:', modules, modules['AppModule']);
         this.cfr = this.compiler.compileModuleAndAllComponentsSync(modules['AppModule']);
         console.log(this.cfr,',', this.cfr.componentFactories[0]);
         this.putStuffHere.createComponent(this.cfr.componentFactories[0], 0);
       });
-    // this.putStuffHere.createComponent(this.cfr, 0);
-    
-    // const resolve = (comp:any) => Observable.fromPromise(this.cfr.resolveComponentFactory(comp));
     
     
   }
